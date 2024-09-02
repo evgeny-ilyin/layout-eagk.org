e35ac538702c8dc63015bb6f3ba93e7ccdd4e0d7
Evgeny Ilyin <evgeniy.v.ilyin@gmail.com>
2024-09-02 22:33:57 +0300

    calendar tune

--- src/js/modules/calendar.js
+++ src/js/modules/calendar.js
@@ -1,33 +1,36 @@
 import VanillaCalendar from "vanilla-calendar-pro";
 
 addEventListener("DOMContentLoaded", () => {
-	const options = {
-		settings: {
-			lang: "ru",
-			visibility: {
-				daysOutside: false,
+	const calendar = document.querySelector(".calendar"),
+		options = {
+			settings: {
+				lang: "ru",
+				visibility: {
+					daysOutside: false,
+				},
+				selection: {
+					day: false,
+				},
 			},
-			selection: {
-				day: false,
+			date: {
+				min: "2024-05-01",
+				max: "2034-04-30",
 			},
-		},
-		date: {
-			min: "2024-05-01",
-			max: "2034-04-30",
-		},
-		actions: {
-			clickMonth(e, self) {
-				// fetch
-				console.log(self.selectedMonth + 1);
+			actions: {
+				clickMonth(e, self) {
+					// fetch
+					// console.log(self.selectedMonth + 1);
+				},
 			},
-		},
-	};
+		};
 
-	window.calendar = new VanillaCalendar(".calendar", options);
-	calendar.init();
+	if (!calendar) return;
+
+	window.cal = new VanillaCalendar(calendar, options);
+	cal.init();
 
 	// при загрузке дергать текущий месяц из базы и обновлять тут
 
-	calendar.selectedDates = ['2024-08-01-2024-08-03', '2024-08-10', '2024-08-12', '2024-08-15', '2024-08-31', '2024-09-27'];
-	calendar.update();
+	cal.selectedDates = ["2024-08-01-2024-08-03", "2024-08-10", "2024-08-12", "2024-08-15", "2024-08-31", "2024-09-27"];
+	cal.update();
 });
