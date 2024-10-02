1be2794f38bab5738118a2c552d43d0d396efb5f
Evgeny Ilyin <evgeniy.v.ilyin@gmail.com>
2024-10-02 09:30:29 +0300

    DOMContentLoaded for map

--- src/js/modules/yandex-map.js
+++ src/js/modules/yandex-map.js
@@ -1,43 +1,45 @@
-const mapContainer = document.querySelector(".map-resort");
+addEventListener("DOMContentLoaded", () => {
+	const mapContainer = document.querySelector(".map-resort");
 
-if (!window.mapInit) {
-	window.mapInit = () => {
-		if (!mapContainer) return;
+	if (!window.mapInit) {
+		window.mapInit = () => {
+			if (!mapContainer) return;
 
-		let pointerIcon = data.pointerIcon,
-			zoomDefault = 10,
-			options = { suppressMapOpenBlock: true, minZoom: 5, maxZoom: 16 };
+			let pointerIcon = data.pointerIcon,
+				zoomDefault = 10,
+				options = { suppressMapOpenBlock: true, minZoom: 5, maxZoom: 16 };
 
-		const map = new ymaps.Map(
-			mapContainer,
-			{
-				center: [0, 0],
-				zoom: zoomDefault,
-				controls: ["zoomControl", "fullscreenControl", "typeSelector"],
-			},
-			options
-		);
+			const map = new ymaps.Map(
+				mapContainer,
+				{
+					center: [0, 0],
+					zoom: zoomDefault,
+					controls: ["zoomControl", "fullscreenControl", "typeSelector"],
+				},
+				options
+			);
 
-		let objectManager = new ymaps.ObjectManager({});
+			let objectManager = new ymaps.ObjectManager({});
 
-		objectManager.objects.options.set({
-			preset: "islands#blueDotIconWithCaption",
-			// iconLayout: "default#image",
-			// iconImageHref: pointerIcon,
-			// iconImageSize: [45, 45],
-			// balloonMaxWidth: 254,
-		});
+			objectManager.objects.options.set({
+				preset: "islands#blueDotIconWithCaption",
+				// iconLayout: "default#image",
+				// iconImageHref: pointerIcon,
+				// iconImageSize: [45, 45],
+				// balloonMaxWidth: 254,
+			});
 
-		objectManager.add(data);
+			objectManager.add(data);
 
-		map.geoObjects.add(objectManager);
-		// zoomMargin нужен, если точки при getBounds оказались слишком близко к краю прямоугольника
-		map.setBounds(objectManager.getBounds(), { checkZoomRange: true, zoomMargin: 10 }).then(function () {
-			if (map.getZoom() > zoomDefault) map.setZoom(zoomDefault);
-		});
-	};
-}
+			map.geoObjects.add(objectManager);
+			// zoomMargin нужен, если точки при getBounds оказались слишком близко к краю прямоугольника
+			map.setBounds(objectManager.getBounds(), { checkZoomRange: true, zoomMargin: 10 }).then(function () {
+				if (map.getZoom() > zoomDefault) map.setZoom(zoomDefault);
+			});
+		};
+	}
 
-if(mapContainer) {
-	ymaps.ready(mapInit);
-}
+	if (mapContainer) {
+		ymaps.ready(mapInit);
+	}
+});
