bb82df6c5a0cd6ee0fe7ba282852f8990f449d2c
Evgeny Ilyin <evgeniy.v.ilyin@gmail.com>
2025-05-21 14:32:09 +0300

    removed temp

--- temp/lang/en/footer.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["SECRETARIAT"] = "Secretary";
-$MESS["ADDRESS"] = "Address";
-$MESS["HOW_TO_FIND"] = "How to find us";
-$MESS["CONTACT_US"] = "Contact us";
-?>
--- temp/lang/en/header.php
+++
@@ -1,4 +0,0 @@
-<?
-$MESS["ARIA_MAINPAGE"] = "Main page";
-$MESS["LOGIN"] = "Login to your account";
-?>
